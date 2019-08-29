989285a4c6feeebe380be67b289faf23436c486a
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-29 23:29:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -193,9 +193,7 @@ def big_shoe_rebounds
   big_shoe = 0
   big_shoe_player = ""
   hash.each do |base, info|
-    binding.pry
     hash[base][:players].each do |player, stats|
-      binding.pry
       if stats[:shoe] > big_shoe
         big_shoe = stats[:shoe]
         big_shoe_player = player
