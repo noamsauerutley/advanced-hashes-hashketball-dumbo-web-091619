2568f9e7c53312f815112a2dd4cf10389bd1d635
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-30 00:12:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -236,12 +236,13 @@ def winning_team
 end
 
 def player_with_longest_name
+  player_names = []
   longest_length = 0
   longest_name = ""
   hash = game_hash
   hash.each do |base, info|
-    info[:players].each do |player|
-      if player.to_s.length > longest_length
+    player_names.push(info[:players].keys)
+      if playerlength > longest_length
         longest_length = player.to_s.length
         longest_name = player.to_s
       end
